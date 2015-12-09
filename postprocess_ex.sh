22b95d0e217ee4566c7448bd1570ef757852f71b
Andy Aschwanden <aaschwanden@alaska.edu>
2015-12-09 13:57:16 -0900

    working on post processing

---
+++ postprocess_ex.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -x -e
+bagrid=bamber5km.nc
+create_greenland_bamber_grid.py -g 5000 $bagrid
+nc2cdo.py $bagrid
+N=4
+method=bil
+rmweights=remapweigths.nc
+sgrid=sgrid.nc
+
+inprefix=ex_greenland_g3600m_const_ctrl_v2_sia_e_1.25_ppq_0.6_tefo_0.02_ssa_n_3.25_ssa_e_1.0_phi_min_5.0_phi_max_40.0_topg_min_-700_topg_max_700_hydro_100a
+ncks --64 -O -v thk ${inprefix}_ctrl.nc $sgrid
+nc2cdo.py $sgrid
+cdo -P $N gen$method,$bagrid $sgrid $rmweights
+
+for exp in "ctrl" "asmb"; do
+    infile=${inprefix}_${exp}.nc
+    tmpfile1=tmp_$infile
+    ncks --64 -O -v mapping,thk,topg,usurf,climatic_mass_balance,bmelt,uvelsurf,vvelsurf,discharge_flux_cumulative,tempsurf,taub_mag,mask $infile $tmpfile1
+    ncap2 -O -s '*sz_idt=time.size(); *discharge_flux[$time,$y,$x]= 0.f; *dHdt[$time,$y,$x]= 0.f; for(*idt=1 ; idt<sz_idt ; idt++) {discharge_flux(idt,:,:)=-(discharge_flux_cumulative(idt,:,:)-discharge_flux_cumulative(idt-1,:,:))/(time(idt)-time(idt-1))*1e12; dHdt(idt,:,:)=(thk(idt,:,:)-thk(idt-1,:,:))/(time(idt)-time(idt-1));} discharge_flux.ram_write(); dHdt.ram_write(); uvelsurf=uvelsurf/31556925.9747; vvelsurf=vvelsurf/31556925.9747; climatic_mass_balance=climatic_mass_balance/31556925.9747; sftgif[$time,$y,$x]=0b; where(mask==2 || mask==3) sftgif=1; sftflf[$time,$y,$x]=0b; where(mask==3) sftflf=1; sfrgrf[$time,$y,$x]=0b; where(mask==2) sfrgrf=1;' $tmpfile1 $tmpfile1
+    ncatted -a units,discharge_flux,o,c,"kg s-1" -a units,dHdt,o,c,"m s-1" -a units,uvelsurf,o,c,"m s-1" -a units,vvelsurf,o,c,"m s-1" -a units,climatic_mass_balance,o,c,"kg m-2 s-1" $tmpfile1
+    ncks -O -x -v mask,discharge_flux_cumulative $tmpfile1 $tmpfile1
+    nc2cdo.py $tmpfile1
+    tmpfile2=tmp2_$infile
+    cdo timselavg,5 $tmpfile1 $tmpfile2
+    ncrename -v thk,lithk -v usurf,orog -v climatic_mass_balance,acabf -v bmelt,libmassbf  -v tempsurf,litempsnic -v taub_mag,strbasemag -v discharge_flux,licalvf $tmpfile2
+    ncatted -a standard_name,acabf,o,c,"land_ice_surface_specific_mass_balance_flux"  -a standard_name,dHdt,o,c,"tendency_of_land_ice_thickness" -a standard_name,libmassbf,o,c,"land_ice_basal_specific_mass_balance_flux" -a standard_name,licalvf,o,c," 	land_ice_specific_mass_flux_due_to_calving" -a standard_name,sftgif,o,c,"land_ice_area_fraction" -a standard_name,sfrgrf,o,c,"grounded_ice_sheet_area_fraction" -a standard_name,sftflf,o,c,"floating_ice_sheet_area_fraction" -a standard_name,strbasemag,o,c,"magnitude_of_land_ice_basal_drag" -a standard_name,litempsnic,o,c,"temperature_at_ground_level_in_snow_or_firn" $tmpfile2
+    expu=$(echo $exp | tr '[:lower:]' '[:upper:]')
+    obase=GIS_UAF_PISM_${expu}
+    cdo -P $N remap,$bagrid,$rmweights $tmpfile2 $outfile
+    ncks -A -v x,y,mapping $bagrid $outfile
+    cdo splitname,swap $outfile $obase
+    for file in *_$obase.nc; do
+        ncks -A -v x,y,mapping $bagrid $file
+    done
+done
--- postprocess_ts.sh
+++ postprocess_ts.sh
@@ -8,7 +8,7 @@ for exp in "ctrl" "asmb"; do
     # expu=${exp^^}
     expu=$(echo $exp | tr '[:lower:]' '[:upper:]')
     outfile=scalar_GIS_UAF_PISM_${expu}.nc
-    cdo yearmean -selvar,imass,discharge_flux,grounded_basal_ice_flux,surface_ice_flux,iareag,iareaf $infile $outfile
+    cdo yearmean -selvar,imass,discharge_flux,grounded_basal_ice_flux,surface_ice_flux,nonneg_flux,iareag,iareaf $infile $outfile
     ncrename -v imass,lim -v discharge_flux,tendacabf -v surface_ice_flux,tendlibmassbf $outfile
     ncatted -a standard_name,lim,o,c,"land_ice_mass" -a standard_name,iareag,o,c,"grounded_land_ice_area" -a standard_name,iareaf,o,c,"floating_ice_shelf_area" -a standard_name,tendacabf,o,c,"tendency_of_land_ice_mass_due_to_surface_mass_balance" -a standard_name,tendlibmassbf,o,c,"tendency_of_land_ice_mass_due_to_basal_mass_balance" $outfile
 done
